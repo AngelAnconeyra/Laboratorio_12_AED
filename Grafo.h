0779f1c4c1622b79451d3718dec662412ba3c3d6
SamuelChambiYtusaca <schambiyt@unsa.edu.pe>
2021-12-21 21:46:45 -0500

    :alien:

--- Grafo.h
+++ Grafo.h
@@ -1,10 +1,6 @@
 #ifndef _GRAFO_H_
 #define _GRAFO_H_
 #include <bits/stdc++.h>
-#include<sstream>
-#include<fstream>
-#include<list>
-#include<vector>
 #include "Nodo.h"
 using namespace std;
 template<class T, class A>
@@ -19,12 +15,17 @@ class Grafo{
         void borrarNodo(T dato);
         void borrarArista(T n1, T n2);
         bool existeNodo(T);
+        bool existeArista(T,T);
+        A val_Arista(T,T);
         int index_vert(T nombre);
         vector<vector<int>> matrixAdyacencias();
         vector<int> CalcularIndegree(vector<vector<int>>);
         int EncuentraI0(vector<int>);
         void DecrementaIndigree(vector<vector<int>>,vector<int> &,int);
         void ordenTopologico(vector<vector<int>>,vector<int>&);
+        T dist_minima(map<T,bool>, map<T,int>);
+        void dijkstra(T);
+        void dijkstra(map<T,bool> &visitados, map<T,int> &distancias, T src);
         void print(string);
         void print(ofstream &);
 };
@@ -108,11 +109,38 @@ bool Grafo<T,A>::existeNodo(T dat){
 }
 
 template<class T, class A>
+bool Grafo<T,A>::existeArista(T dat1, T dat2){
+    for(list<Node<T,A>> &a: vertices){
+        if( (*a.begin()).dato == dat1){
+            for(typename list<Node<T,A>>::iterator i = ++a.begin(); i!=a.end();++i){
+                if( (*i).dato == dat2){
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+template<class T, class A>
+A Grafo<T,A>::val_Arista(T dat1, T dat2){
+    for(list<Node<T,A>> &a: vertices){
+        if( (*a.begin()).dato == dat1){
+            for(typename list<Node<T,A>>::iterator i = ++a.begin(); i!=a.end();++i){
+                if( (*i).dato == dat2){
+                    return (*i).ValArista;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+template<class T, class A>
 int Grafo<T,A>::index_vert(T nombre){
     int index = 0;
     for(list<Node<T,A>> a : vertices){
-        if (nombre == (*a.begin()).dato)
-        {
+        if (nombre == (*a.begin()).dato){
             return index;
         }
         index++;
@@ -202,17 +230,72 @@ void Grafo<T,A>::ordenTopologico(vector<vector<int>> matrix,vector<int> & indegr
     }while(nodo != -1);
 }
 
+string space(string a){
+    string ans = a;
+    for(int i=0;i<a.size();++i){
+        if(a[i]==' ') ans[i] = '_';
+    }
+    return ans;
+}
+
+template<class T, class A>
+T Grafo<T,A>::dist_minima(map<T,bool> visitados, map<T,int>distancias){
+    int mini = INT_MAX;
+    T ans;
+    for (typename vector<list<Node<T,A>>>::iterator it = vertices.begin(); it!= vertices.end(); it++) {
+        if(visitados[(*((it)->begin())).dato]==false && distancias[(*((it)->begin())).dato] <= mini){
+            mini = distancias[(*((it)->begin())).dato];
+            ans = (*((it)->begin())).dato ; 
+        }
+    }
+    return ans;
+}
+
+template<class T, class A>
+void Grafo<T,A>::dijkstra(map<T,bool> &visitados, map<T,int> &distancias, T src){
+    for (typename vector<list<Node<T,A>>>::iterator it = vertices.begin(); it!= vertices.end(); it++) {
+        distancias[(*((it)->begin())).dato] = INT_MAX;
+        visitados[(*((it)->begin())).dato]  = false;
+    }
+    distancias[src] = 0;
+    for (typename vector<list<Node<T,A>>>::iterator it2 = vertices.begin(); it2 != vertices.end(); it2++) {
+        T val = dist_minima(visitados, distancias);
+        
+        visitados[val] = true;
+        for (typename vector<list<Node<T,A>>>::iterator it2 = vertices.begin(); it2 != vertices.end(); it2++){
+            if(!visitados[(*((it2)->begin())).dato] && existeArista((*((it2)->begin())).dato,val) && distancias[val]!=INT_MAX && distancias[val] + val_Arista((*((it2)->begin())).dato,val) < distancias[(*((it2)->begin())).dato])
+            {
+                distancias[(*((it2)->begin())).dato] = distancias[val] + val_Arista((*((it2)->begin())).dato, val);
+            }
+        }
+    }
+
+    cout << "Nodo: " << "\t\t\t\t\t\t\t\t\t\t" << "Distancia Minima: \n";
+    for (typename vector<list<Node<T,A>>>::iterator it = vertices.begin(); it!= vertices.end(); it++){
+        cout << (*((it)->begin())).dato << "\t\t\t\t\t\t\t\t\t"; distancias[(*((it)->begin())).dato]!=INT_MAX ? cout << distancias[(*((it)->begin())).dato] << "\n" : cout << "0" << "\n";
+    }
+    return;
+}
+
+template<class T, class A>
+void Grafo<T,A>::dijkstra(T src){
+    map<T, bool> visitados;
+    map<T, int>  distancias;
+    dijkstra(visitados, distancias, src); 
+}   
+
 template<class T, class A>
 void Grafo<T,A>::print(ofstream &os) {
     for(list<Node<T,A>> a : vertices)
         os << (*a.begin()).dato << ";\n";
     for(list<Node<T,A>> a : vertices){
         for(Node<T,A> nodo : a){
-            if(nodo.dato != (*a.begin()).dato ) 
+            if( nodo.dato != (*a.begin()).dato ) 
                 os << (*a.begin()).dato << "->" << nodo.dato << " [ label = " << nodo.ValArista << "];\n";
         }
     }
 }
+
 
 template<class T, class A>
 void Grafo<T,A>::print(string file){
